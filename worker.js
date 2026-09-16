82faaf765f32941798572feef64131f4d591f288
gutenmorgantoyou <gutenmorgantoyou@gmail.com>
2026-09-16 14:26:39 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -109,7 +109,7 @@ export default {
         ]
       }));
 
-      const model = "gemini-2.5-flash";
+      const model = "gemini-3.6-flash";
 
       const url =
         `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${env.GEMINI_API_KEY}`;
